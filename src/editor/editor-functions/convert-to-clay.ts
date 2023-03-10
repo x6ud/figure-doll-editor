c69024fe789dd73b19800ab70a2811c309e8e472
x6ud <x6ud@foxmail.com>
2023-03-10 09:39:05 +0800

    fixed: convert to clay

--- src/editor/editor-functions/convert-to-clay.ts
+++ src/editor/editor-functions/convert-to-clay.ts
@@ -1,4 +1,4 @@
-import {BufferGeometry, Mesh, Vector3} from 'three';
+import {Euler, Matrix4, Mesh, Object3D, Quaternion, Vector3} from 'three';
 import {toRaw} from 'vue';
 import EditorContext from '../EditorContext';
 import CCastShadow from '../model/components/CCastShadow';
@@ -14,140 +14,102 @@ import CReceiveShadow from '../model/components/CReceiveShadow';
 import CRotation from '../model/components/CRotation';
 import CRoughness from '../model/components/CRoughness';
 import CScale from '../model/components/CScale';
-import CSdfDirty from '../model/components/CSdfDirty';
 import CSymmetry from '../model/components/CSymmetry';
 import CVertices from '../model/components/CVertices';
 import ModelNode from '../model/ModelNode';
 import ModelNodeComponent from '../model/ModelNodeComponent';
 import Class from '../type/Class';
 
+const _position = new Vector3();
+const _rotation = new Quaternion();
+const _scale = new Vector3();
+const _invMat = new Matrix4();
+const _mat = new Matrix4();
+
 export function convertModelNodeToClay(ctx: EditorContext, node: ModelNode) {
     node = toRaw(node);
     const parent = node.parent;
-    let verticesArr: Float32Array[] = [];
-    let colorsArr: (Float32Array | null)[] = [];
-    switch (node.type) {
-        case 'Shape': {
-            const cSdfDirty = node.get(CSdfDirty);
-            if (cSdfDirty.throttleHash) {
-                const task = ctx.throttleTasks.get(cSdfDirty.throttleHash);
-                if (task) {
-                    task.callback();
-                    ctx.throttleTasks.delete(cSdfDirty.throttleHash);
-                }
-                cSdfDirty.throttleHash = '';
-            }
-            const mesh = node.value(CObject3D) as Mesh;
-            const geometry = mesh.geometry;
-            verticesArr = [new Float32Array(geometry.getAttribute('position').array)];
-            if (geometry.hasAttribute('color')) {
-                colorsArr = [new Float32Array(geometry.getAttribute('color').array)];
-            } else {
-                colorsArr = [null];
-            }
-        }
-            break;
-        default: {
-            if (!node.has(CObject3D)) {
-                return;
-            }
-            const mesh = node.value(CObject3D);
-            if (!mesh) {
-                return;
-            }
-            const geometries: BufferGeometry[] = [];
-            if ((mesh as Mesh).geometry) {
-                geometries.push((mesh as Mesh).geometry);
-            } else {
-                for (let child of mesh.children) {
-                    if ((child as Mesh).geometry) {
-                        geometries.push((child as Mesh).geometry);
-                    }
-                }
-            }
-            const _a = new Vector3();
-            const _b = new Vector3();
-            const _c = new Vector3();
-            for (let geometry of geometries) {
-                if (!geometry.isBufferGeometry) {
-                    continue;
-                }
-                const attrPos = geometry.getAttribute('position');
-                if (!attrPos) {
-                    continue;
-                }
-                const index = geometry.index;
-                if (index) {
-                    const vertices = new Float32Array(index.count * 3);
-                    const colors = new Float32Array(index.count * 3);
-                    for (let i = 0, len = index.count; i < len; i += 3) {
-                        _a.fromBufferAttribute(attrPos, index.getX(i));
-                        _b.fromBufferAttribute(attrPos, index.getX(i + 1));
-                        _c.fromBufferAttribute(attrPos, index.getX(i + 2));
-                        const j = i * 3;
-                        vertices[j] = _a.x;
-                        vertices[j + 1] = _a.y;
-                        vertices[j + 2] = _a.z;
-                        vertices[j + 3] = _b.x;
-                        vertices[j + 4] = _b.y;
-                        vertices[j + 5] = _b.z;
-                        vertices[j + 6] = _c.x;
-                        vertices[j + 7] = _c.y;
-                        vertices[j + 8] = _c.z;
-                    }
-                    if (geometry.hasAttribute('color')) {
-                        const attrColor = geometry.getAttribute('color');
-                        for (let i = 0, len = index.count; i < len; i += 3) {
-                            _a.fromBufferAttribute(attrColor, index.getX(i));
-                            _b.fromBufferAttribute(attrColor, index.getX(i + 1));
-                            _c.fromBufferAttribute(attrColor, index.getX(i + 2));
-                            const j = i * 3;
-                            colors[j] = _a.x;
-                            colors[j + 1] = _a.y;
-                            colors[j + 2] = _a.z;
-                            colors[j + 3] = _b.x;
-                            colors[j + 4] = _b.y;
-                            colors[j + 5] = _b.z;
-                            colors[j + 6] = _c.x;
-                            colors[j + 7] = _c.y;
-                            colors[j + 8] = _c.z;
-                        }
-                    } else {
-                        for (let i = 0, len = colors.length; i < len; ++i) {
-                            colors[i] = 1;
-                        }
-                    }
-                    if (vertices.length) {
-                        verticesArr.push(vertices);
-                        colorsArr.push(colors);
-                    }
-                } else {
-                    if (attrPos.array.length) {
-                        verticesArr.push(new Float32Array(attrPos.array));
-                        if (geometry.hasAttribute('color')) {
-                            const attrColor = geometry.getAttribute('color');
-                            colorsArr.push(new Float32Array(attrColor.array));
-                        } else {
-                            colorsArr.push(null);
-                        }
-                    }
-                }
-            }
-        }
-            break;
-    }
-    if (!verticesArr.length) {
+    if (!node.has(CObject3D)) {
         return;
     }
-    for (let i = 0, len = verticesArr.length; i < len; ++i) {
-        const vertices = verticesArr[i];
-        const colors = colorsArr[i];
-        const data: { [name: string]: any } = {
-            [CVertices.name]: vertices,
-            [CColors.name]: colors || undefined,
-        };
+    const object3D = node.value(CObject3D);
+    if (!object3D) {
+        return;
+    }
+    _invMat.copy(node.getParentWorldMatrix()).invert();
+    const stack: Object3D[] = [object3D];
+    const meshes: Mesh[] = [];
+    while (stack.length) {
+        const object3D = stack.pop();
+        if (!object3D) {
+            break;
+        }
+        const objNode = object3D.userData.node as ModelNode | undefined;
+        if (objNode && objNode.id !== node.id) {
+            continue;
+        }
+        if (!object3D.visible) {
+            continue;
+        }
+        if ((object3D as Mesh).isMesh) {
+            meshes.push(object3D as Mesh);
+        }
+        stack.push(...object3D.children);
+    }
+    for (let mesh of meshes) {
+        const geometry = mesh.geometry;
+        const vertices: number[] = [];
+        let colors: number[] | null = null;
+        if (geometry.index) {
+            const aPos = geometry.getAttribute('position');
+            if (aPos?.itemSize !== 3) {
+                continue;
+            }
+            const indices = geometry.index.array;
+            const arrPos = aPos.array;
+            for (let j = 0, len = indices.length; j < len; ++j) {
+                const i = indices[j];
+                const k = i * 3;
+                vertices.push(arrPos[k], arrPos[k + 1], arrPos[k + 2]);
+            }
+            const aCol = geometry.getAttribute('color');
+            if (aCol) {
+                const arrCol = aCol.array;
+                colors = [];
+                for (let j = 0, len = indices.length, step = aCol.itemSize; j < len; ++j) {
+                    const i = indices[j];
+                    const k = i * step;
+                    colors.push(arrCol[k], arrCol[k + 1], arrCol[k + 2]);
+                }
+            }
+        } else {
+            const aPos = geometry.getAttribute('position');
+            if (aPos?.itemSize !== 3) {
+                continue;
+            }
+            const arrPos = aPos.array;
+            for (let i = 0, len = arrPos.length; i < len; i += 3) {
+                vertices.push(arrPos[i], arrPos[i + 1], arrPos[i + 2]);
+            }
+            const aCol = geometry.getAttribute('color');
+            if (aCol) {
+                colors = [];
+                const arrCol = aCol.array;
+                for (let i = 0, len = arrCol.length, step = aCol.itemSize; i < len; i += step) {
+                    colors.push(arrCol[i], arrCol[i + 1], arrCol[i + 2]);
+                }
+            }
+        }
+        if (!vertices.length) {
+            continue;
+        }
+        const data: { [name: string]: any } = {};
+        data[CVertices.name] = new Float32Array(vertices);
+        if (colors) {
+            data[CColors.name] = new Float32Array(colors);
+        }
         const classes: Class<ModelNodeComponent<any>>[] = [
-            CName, CCastShadow, CReceiveShadow, CPosition, CRotation, CScale,
+            CName, CCastShadow, CReceiveShadow,
             CRoughness, CMetalness, CColor, CEmissive, COpacity, CSymmetry,
         ];
         for (let componentClass of classes) {
@@ -155,11 +117,15 @@ export function convertModelNodeToClay(ctx: EditorContext, node: ModelNode) {
                 data[componentClass.name] = node.cloneValue(componentClass);
             }
         }
-        ctx.history.createNode({
-            type: 'Clay',
-            parentId: parent ? parent.id : 0,
-            data,
-        });
+        mesh.updateWorldMatrix(true, false);
+        _mat.copy(mesh.matrixWorld);
+        _mat.multiplyMatrices(_invMat, _mat);
+        _mat.decompose(_position, _rotation, _scale);
+        _rotation.identity();
+        data[CPosition.name] = new Vector3().copy(_position);
+        data[CRotation.name] = new Euler().setFromQuaternion(_rotation);
+        data[CScale.name] = _scale.x;
+        ctx.history.createNode({type: 'Clay', parentId: parent?.id, data});
     }
     ctx.history.removeNode(node.id);
 }
